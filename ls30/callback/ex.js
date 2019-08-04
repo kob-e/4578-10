360762a33104d032fb62334c2899107bb9dbf19c
kob-e <kobieshka@gmail.com>
2019-08-04 21:15:03 +0300

    fix ex

--- ls30/callback/ex.js
+++ ls30/callback/ex.js
@@ -1,20 +1,21 @@
+function sendToWeather(selectedCity) {
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=' + selectedCity + '&units=metric&appid=1e8ffdb64b7d4d8dbdd4bf3a700bdeb0';
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function (aEvt) {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                var obj = JSON.parse(this.responseText)
+                console.log(obj);
+                weather.innerHTML += '<div class="box">' + obj.name + ': ' + obj.main.temp + '<div>';
+            }
+        }
+    };
+    req.open("GET", url);
+    req.send();
+}
 
-function reqListener () {
-    var obj = JSON.parse(this.responseText)
-    console.log(obj);
-    weather.innerHTML += '<div class="box">' + obj.name + ': ' + obj.main.temp + '<div>';
-  }
 
-  function sendToWeather(selectedCity) {
-    url = 'http://api.openweathermap.org/data/2.5/weather?q='+selectedCity+'&units=metric&appid=1e8ffdb64b7d4d8dbdd4bf3a700bdeb0';
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
-    oReq.open("GET", url);
-    oReq.send();
-  }
-
-
-  sendToWeather('jerusalem');
-  sendToWeather('paris');
-  sendToWeather('stockholm');
-  sendToWeather('berlin');
+sendToWeather('jerusalem');
+sendToWeather('paris');
+sendToWeather('stockholm');
+sendToWeather('berlin');
