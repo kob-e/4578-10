d00dd3cc6ff316e098070f90d478c67c9f4fdd92
kob-e <kobieshka@gmail.com>
2019-11-06 21:23:44 +0200

    added logging middleware

--- ls50/index.js
+++ ls50/index.js
@@ -3,10 +3,23 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const PORT = 3201;
-const phoneBl = require('./phonebl');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    if (process.env.debug) {
+        console.log({
+            method: req.method,
+            path: req.path,
+            originalUrl: req.originalUrl,
+            body: req.body,
+            params: req.params,
+            query: req.query,
+            url: req.url
+        })
+    }
+    next();
+});
 
 const phoneRouter = require('./routes/phone');
 
--- ls50/routes/phone.js
+++ ls50/routes/phone.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const phoneBl = require('./../phonebl');
 
 router.get('/', (req, res) => {
 
